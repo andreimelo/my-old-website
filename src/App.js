01c8022f1a5ab1394896c709997bbf57291029f2
andreimelo <meloandrei69@gmail.com>
2019-01-13 20:50:24 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import WebUnderConstruction from './component/assets/website_under_construction.png'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <div className="andrei_melo_container">
+      <img src={WebUnderConstruction} className="web_image_size" />
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
